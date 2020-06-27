08511c6bff3a50ec53f4e226f8c77934f28721e2
Frank <frankgoyens@hotmail.com>
2020-06-27 16:47:46 +0200

    Activating the table row changes the directory
    Changing to parent can be done from the alt+key_up shortcut or the '..' entry in the table view

--- TabsPls_Qt/LightSpeedFileExplorer/TabsPlsMainWindow.cpp
+++ TabsPls_Qt/LightSpeedFileExplorer/TabsPlsMainWindow.cpp
@@ -12,6 +12,38 @@
 #include "FileListTableView.hpp"
 #include "FileListViewModel.hpp"
 
+static void ChangeDirectoryWithoutHistoryUpdate(const FileSystem::Directory& newDir, FileListViewModel& model, QLineEdit& directoryInputField)
+{
+	const auto newDirString = QString::fromStdString(newDir.path());
+	directoryInputField.setText(newDirString);
+	model.ChangeDirectory(newDirString);
+}
+
+static auto CreateDirectoryChangedByGoingToParent(RobustDirectoryHistoryStore& history, FileListViewModel& model, QLineEdit& directoryInputField)
+{
+	return [&]()
+	{
+		try
+		{
+			const auto newDir = history.GetCurrent().Parent();
+			if (newDir.path() == history.GetCurrent().path())
+				return;
+			history.OnNewDirectory(newDir);
+			ChangeDirectoryWithoutHistoryUpdate(newDir, model, directoryInputField);
+		}
+		catch (const StoreIsEmptyException&) {}
+	};
+}
+
+static auto CreateDirectoryChangedFromTableClosure(RobustDirectoryHistoryStore& history, FileListViewModel& model, QLineEdit& directoryInputField)
+{
+	return [&](const FileSystem::Directory& newDir)
+	{
+		history.OnNewDirectory(newDir);
+		ChangeDirectoryWithoutHistoryUpdate(newDir, model, directoryInputField);
+	};
+}
+
 namespace
 {
 	struct HistoryBackVariant {};
@@ -36,9 +68,7 @@ static auto CreateHistoryActionClosure(RobustDirectoryHistoryStore& history, Fil
 		try
 		{
 			SwitchHistory(history, variant);
-			const auto newDir = QString::fromStdString(history.GetCurrent().path());
-			directoryInputField.setText(newDir);
-			model.ChangeDirectory(newDir);
+			ChangeDirectoryWithoutHistoryUpdate(history.GetCurrent(), model, directoryInputField);
 		}
 		catch (const ImpossibleSwitchException&) {}
 	};
@@ -115,4 +145,21 @@ TabsPlsMainWindow::TabsPlsMainWindow(const QString& initialDirectory)
 
 	connect(backButton, &QPushButton::pressed, backActionClosure);
 	connect(forwardButton, &QPushButton::pressed, forwardActionClosure);
+
+	const auto directoryChangedByGoingToParentClosure = CreateDirectoryChangedByGoingToParent(m_historyStore, *fileListViewModel, *topBarDirectoryInputField);
+	const auto directoryChangedFromTableClosure = CreateDirectoryChangedFromTableClosure(m_historyStore, *fileListViewModel, *topBarDirectoryInputField);
+
+	connect(fileListViewWidget, &QTableView::activated, [=](const QModelIndex& index)
+	{
+		const auto dirString = fileListViewModel->data(index, Qt::UserRole);
+
+		if (dirString == "..")
+			directoryChangedByGoingToParentClosure();
+		else if (const auto dir = FileSystem::Directory::FromPath(dirString.toString().toStdString()))
+			directoryChangedFromTableClosure(*dir);
+	});
+
+	const auto* gotoParentActionShortcut = new QShortcut(QKeySequence(Qt::ALT + Qt::Key_Up), this);
+
+	connect(gotoParentActionShortcut, &QShortcut::activated, directoryChangedByGoingToParentClosure);
 }
