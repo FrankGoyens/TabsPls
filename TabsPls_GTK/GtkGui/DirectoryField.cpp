eee23b8c2541af622e683281dca9897152c9ce6d
Frank <frankgoyens@hotmail.com>
2020-12-13 21:32:39 +0100

    Cleanup of unused files

--- TabsPls_GTK/GtkGui/DirectoryField.cpp
+++
@@ -1,6 +0,0 @@
-#include "DirectoryField.hpp"
-
-namespace DirectoryField
-{
-    
-}
--- TabsPls_UWP/LightSpeedExplorer/TabsPlsCoreUWP/pch.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-
-#include <unknwn.h>
-#include <winrt/base.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.Foundation.Collections.h>
