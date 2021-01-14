c951d776ff98c5ad21b7c0f82140f6000603133f
Frank <frankgoyens@hotmail.com>
2021-01-14 20:42:51 +0100

    Added some working Python api code
    This is some proof of concept code, to make sure the python module is linked correctly

--- TabsPls_Python/src/TabsPls_Python.cpp
+++ TabsPls_Python/src/TabsPls_Python.cpp
@@ -1,1 +1,23 @@
 #include <TabsPls_Python/TabsPls_Python.hpp>
+
+extern "C" {
+#include <Python.h>
+}
+
+namespace TabsPlsPython {
+void ExecPython(const char* code) {
+    Py_Initialize();
+    PyRun_SimpleString(code);
+    Py_Finalize();
+}
+
+void SendToTrash(const char* file) {
+    Py_Initialize();
+    auto* send2TrashModuleName = PyUnicode_FromString("send2trash");
+    auto* send2TrashModule = PyImport_Import(send2TrashModuleName);
+    auto* send2TrashFunction = PyObject_GetAttrString(send2TrashModule, "send2trash");
+    auto* args = PyTuple_Pack(1, PyUnicode_FromString(file));
+    auto* result = PyObject_CallObject(send2TrashFunction, args);
+    Py_Finalize();
+}
+} // namespace TabsPlsPython
