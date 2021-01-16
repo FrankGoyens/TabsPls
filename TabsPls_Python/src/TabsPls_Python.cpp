7b726bd799dd1e94f4465694cd6a85580ea36657
Frank <frankgoyens@hotmail.com>
2021-01-16 14:14:16 +0100

    Refactored the code by using RAII

--- TabsPls_Python/src/TabsPls_Python.cpp
+++ TabsPls_Python/src/TabsPls_Python.cpp
@@ -11,13 +11,28 @@ void ExecPython(const char* code) {
     Py_Finalize();
 }
 
-void SendToTrash(const char* file) {
-    Py_Initialize();
-    auto* send2TrashModuleName = PyUnicode_FromString("send2trash");
-    auto* send2TrashModule = PyImport_Import(send2TrashModuleName);
+namespace {
+struct AcquiredPyInstance {
+    AcquiredPyInstance() { Py_Initialize(); }
+    ~AcquiredPyInstance() { Py_Finalize(); }
+};
+struct AcquiredPyObject {
+    AcquiredPyObject(PyObject* obj_) : obj(obj_) {}
+    ~AcquiredPyObject() { Py_DECREF(obj); }
+    PyObject* obj;
+};
+} // namespace
+
+static void SendToTrashFromInitializedPy(const char* file) {
+    AcquiredPyObject send2TrashModuleName = PyUnicode_FromString("send2trash");
+    auto* send2TrashModule = PyImport_Import(send2TrashModuleName.obj);
     auto* send2TrashFunction = PyObject_GetAttrString(send2TrashModule, "send2trash");
-    auto* args = PyTuple_Pack(1, PyUnicode_FromString(file));
-    auto* result = PyObject_CallObject(send2TrashFunction, args);
-    Py_Finalize();
+    AcquiredPyObject args = PyTuple_Pack(1, PyUnicode_FromString(file));
+    AcquiredPyObject result = PyObject_CallObject(send2TrashFunction, args.obj);
+}
+
+void SendToTrash(const char* file) {
+    AcquiredPyInstance _;
+    SendToTrashFromInitializedPy(file);
 }
 } // namespace TabsPlsPython
