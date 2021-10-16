733feb9152eaf7d1ace3000c77e06e1a98244fcb
Frank Goyens <frankgoyens@hotmail.com>
2021-10-16 17:10:48 +0200

    Add test that catches and formats a python exception

---
+++ TabsPls_Test/testCatchPythonException.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+namespace CatchPythonExceptionTests {
+struct PythonInstance {
+    PythonInstance() { Py_Initialize(); }
+    ~PythonInstance() { Py_Finalize(); }
+};
+
+static PythonInstance& GetPythonInstance() {
+    static PythonInstance instance;
+    return instance;
+}
+
+static void RaisePythonException() {
+    GetPythonInstance();
+    auto* pGlobal = PyDict_New();
+
+    auto* pModule = PyModule_New("mymod");
+    PyModule_AddStringConstant(pModule, "__file__", "");
+
+    auto* pLocal = PyModule_GetDict(pModule);
+
+    PyRun_String("raise Exception(\"something went wrong\")", Py_file_input, pGlobal, pLocal);
+
+    ASSERT_TRUE(PyErr_Occurred());
+}
+
+TEST(CatchPythonExceptionTest, Catch) {
+    RaisePythonException();
+
+    PyObject *type, *value, *traceback;
+    PyErr_Fetch(&type, &value, &traceback);
+
+    PyErr_NormalizeException(&type, &value, &traceback);
+
+    auto* typeString = PyObject_Str(type);
+    EXPECT_EQ(std::string("<class 'Exception'>"), std::string(PyUnicode_AsUTF8(typeString)));
+    Py_XDECREF(typeString);
+
+    auto* valueString = PyObject_Str(value);
+    EXPECT_EQ(std::string("something went wrong"), std::string(PyUnicode_AsUTF8(valueString)));
+    Py_XDECREF(valueString);
+
+    Py_XDECREF(type);
+    Py_XDECREF(value);
+    Py_XDECREF(traceback);
+
+    PyErr_Clear();
+}
+} // namespace CatchPythonExceptionTests
