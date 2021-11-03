97b90566c44ada6bd08c8c0cae99785b94d3e227
Frank Goyens <frankgoyens@hotmail.com>
2021-11-03 08:47:06 +0100

    Forgot to extend the stub

--- TabsPls_Qt/LightSpeedFileExplorer/AssociatedIconProvider_stub.cpp
+++ TabsPls_Qt/LightSpeedFileExplorer/AssociatedIconProvider_stub.cpp
@@ -6,6 +6,8 @@ bool AssociatedIconProvider::ComponentIsAvailable() { return false; }
 
 AssociatedIconProvider& AssociatedIconProvider::Get() { throw ExplicitStubException{}; }
 
+void AssociatedIconProvider::InitThread() {}
+
 std::optional<QIcon> AssociatedIconProvider::FromPath(const FileSystem::RawPath& path) const {
     throw ExplicitStubException{};
 }
