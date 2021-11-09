5793158bb1f4f50f630ca6a0288cddf4dd36f49c
Frank Goyens <frankgoyens@hotmail.com>
2021-11-09 22:01:36 +0100

    Move autocompletion code to its own module

---
+++ TabsPls_Core/source/DirectoryInputAutoComplete.cpp
@@ -0,0 +1,42 @@
+#include <TabsPlsCore/DirectoryInputAutoComplete.hpp>
+
+#include <algorithm>
+#include <iterator>
+#include <set>
+
+#include <TabsPlsCore/FileSystemDirectory.hpp>
+
+static auto Reverse(const FileSystem::RawPath& rawPath) {
+    return FileSystem::RawPath(rawPath.rbegin(), rawPath.rend());
+}
+
+static auto GetDirectories(const FileSystem::Directory& dir) {
+    std::vector<FileSystem::Directory> directories;
+    for (const auto& item : FileSystem::GetFilesInDirectory(dir)) {
+        if (const auto possibleDir = FileSystem::Directory::FromPath(item)) {
+            directories.emplace_back(*possibleDir);
+        }
+    }
+    return directories;
+}
+
+namespace DirectoryInputAutoComplete {
+std::optional<FileSystem::RawPath> Do(const FileSystem::RawPath& incompletePath) {
+    const auto lastSeparator =
+        std::find(incompletePath.rbegin(), incompletePath.rend(), FileSystem::Separator().front());
+    const FileSystem::RawPath incompleteNewPart = Reverse({incompletePath.rbegin(), lastSeparator});
+    const auto basePathForIncompletePart = incompletePath.substr(0, incompletePath.size() - incompleteNewPart.size());
+    if (const auto baseDir = FileSystem::Directory::FromPath(basePathForIncompletePart)) {
+        const auto directories = GetDirectories(*baseDir);
+        std::set<FileSystem::RawPath> sortedDirectories;
+        std::transform(directories.begin(), directories.end(),
+                       std::inserter(sortedDirectories, sortedDirectories.end()),
+                       [](const auto& dir) { return dir.path(); });
+        auto insertion = sortedDirectories.insert(incompletePath);
+        const auto autoCompleter = std::next(insertion.first) == sortedDirectories.end() ? sortedDirectories.begin()
+                                                                                         : std::next(insertion.first);
+        return *autoCompleter;
+    }
+    return {};
+}
+} // namespace DirectoryInputAutoComplete
