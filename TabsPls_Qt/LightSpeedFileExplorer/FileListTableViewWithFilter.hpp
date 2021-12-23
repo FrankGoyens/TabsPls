522a3b5e6fc11592b0e1654991a82544ee67aa27
Frank Goyens <frankgoyens@hotmail.com>
2021-12-23 23:10:46 +0100

    Use model provider when setting up directory changer closure
    This fixes the application crashing, because otherwise the pointer to the model itself would be part of the closure

--- TabsPls_Qt/LightSpeedFileExplorer/FileListTableViewWithFilter.hpp
+++ TabsPls_Qt/LightSpeedFileExplorer/FileListTableViewWithFilter.hpp
@@ -9,23 +9,26 @@ class FileListTableView;
 class QAbstractTableModel;
 class QLineEdit;
 class FileBrowserViewModelSwitcher;
+class FileBrowserViewModelProvider;
 
 class FileListTableViewWithFilter final : public QWidget {
     Q_OBJECT
 
   public:
-    FileListTableViewWithFilter(std::shared_ptr<CurrentDirectoryFileOp>, QAbstractTableModel& viewModel);
+    FileListTableViewWithFilter(std::shared_ptr<CurrentDirectoryFileOp>,
+                                std::unique_ptr<QAbstractTableModel> viewModel);
     ~FileListTableViewWithFilter();
 
     void RequestFlatModel();
     void RequestHierarchyModel();
 
     FileListTableView& GetFileListTableView() { return *m_fileListTableView; }
+    std::weak_ptr<FileBrowserViewModelProvider> GetModelProvider() const;
 
     void ClearFilter();
 
   private:
     FileListTableView* m_fileListTableView;
     QLineEdit* m_filterField;
-    std::unique_ptr<FileBrowserViewModelSwitcher> m_viewModelSwitcher;
+    std::shared_ptr<FileBrowserViewModelSwitcher> m_viewModelSwitcher;
 };
