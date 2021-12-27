e972503b65779075969f52ab670666a356b61477
Frank Goyens <frankgoyens@hotmail.com>
2021-12-27 23:46:17 +0100

    Catch generic exceptions during file retrieval
    At this point, this is to deal with "access denied" exceptions

--- TabsPls_Qt/LightSpeedFileExplorer/FileRetrievalRunnable.cpp
+++ TabsPls_Qt/LightSpeedFileExplorer/FileRetrievalRunnable.cpp
@@ -3,11 +3,15 @@
 #include "FileRetrievalByDispatch.hpp"
 
 void FileRetrievalRunnable::run() {
-    const auto modelEntries = FileRetrievalByDispatch::AsModelEntries(
-        FileRetrievalByDispatch::RetrieveFiles(m_dir, *m_dispatcher), m_basePath.get(), m_fileIcon.get());
+    try {
+        const auto modelEntries = FileRetrievalByDispatch::AsModelEntries(
+            FileRetrievalByDispatch::RetrieveFiles(m_dir, *m_dispatcher), m_basePath.get(), m_fileIcon.get());
 
-    const FileRetrievalRunnableContainer::NameSortedModelSet sortedModelEntries(
-        modelEntries.begin(), modelEntries.end(), &FileEntryModel::ModelEntryDisplayNameSortingPredicate);
+        const FileRetrievalRunnableContainer::NameSortedModelSet sortedModelEntries(
+            modelEntries.begin(), modelEntries.end(), &FileEntryModel::ModelEntryDisplayNameSortingPredicate);
 
-    emit resultReady(sortedModelEntries, m_dispatcher.get());
+        emit resultReady(sortedModelEntries, m_dispatcher.get());
+    } catch (const std::exception&) {
+        // TODO logging
+    }
 }
