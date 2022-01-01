f64dbde26164ab1409b935234b8798aabafe6294
Frank Goyens <frankgoyens@hotmail.com>
2022-01-01 15:25:16 +0100

    Check whether the QGuiApplication is constructed
    The gui might have already been cleaned up (due to the icons being retrieved from other thread)

--- TabsPls_Qt/LightSpeedFileExplorer/TabsPls_WinIconExtract/AssociatedIconProvider.cpp
+++ TabsPls_Qt/LightSpeedFileExplorer/TabsPls_WinIconExtract/AssociatedIconProvider.cpp
@@ -1,5 +1,7 @@
 #include "../AssociatedIconProvider.hpp"
 
+#include <QGuiApplication>
+
 #include <WinIconExtract.hpp>
 
 bool AssociatedIconProvider::ComponentIsAvailable() { return true; }
@@ -10,6 +12,8 @@ AssociatedIconProvider& AssociatedIconProvider::Get() {
 }
 
 std::optional<QIcon> AssociatedIconProvider::FromPath(const FileSystem::RawPath& path) const {
+    if (QGuiApplication::instance() == nullptr)
+        return {};
     if (const auto pixmap = WinIconExtract::IconInfoAsPixmap(path))
         return QIcon(*pixmap);
     return {};
