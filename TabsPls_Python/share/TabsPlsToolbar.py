87ad469f025e5db6da4aedf36cc60dd7c7d70328
Frank Goyens <frankgoyens@hotmail.com>
2023-01-02 20:43:31 +0100

    Remove redundant instance of toolbar plugin base

--- TabsPls_Python/share/TabsPlsToolbar.py
+++
@@ -1,2 +0,0 @@
-ACTIVATION_METHOD_TYPES = {"regular": "regular", "alternative": "alternative"}
-ACTIVATION_RESULT_ACTIONS = {"change_dir": "change_dir", "open_dir_in_tab":"open_dir_in_tab", "message": "message"}
