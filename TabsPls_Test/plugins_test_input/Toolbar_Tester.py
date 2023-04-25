2809599603aec42582a2101513eb9e459f2193f4
Frank Goyens <frankgoyens@hotmail.com>
2023-04-25 23:07:22 +0200

    Simplify test setup for toolbars
    also move TabsPlsToolbar base module to a more appropriate place

--- TabsPls_Test/plugins_test_input/Toolbar_Tester.py
+++ TabsPls_Test/plugins_test_input/Toolbar_Tester.py
@@ -1,4 +1,14 @@
-import TabsPlsToolbar
+# these are usually defined in a separate file TabsPlsToolbar.py which is then imported
+# but that is not necessary for testing purposes,
+# there shouldn't be more than one TabsPlsToolbar.py in the repository as this defines a protocol
+class TabsPlsToolbar(object):
+	ACTIVATION_METHOD_TYPES_REGULAR = "regular"
+	ACTIVATION_METHOD_TYPES_ALTERNATIVE = "alternative"
+
+	ACTIVATION_RESULT_ACTION_CHDIR = "change_dir"
+	ACTIVATION_RESULT_ACTION_OPEN_DIR_IN_TAB = "open_dir_in_tab"
+	ACTIVATION_RESULT_ACTION_MESSAGE = "message"
+##
 
 items = {
     "abc": {"display_name": "/home/user"},
