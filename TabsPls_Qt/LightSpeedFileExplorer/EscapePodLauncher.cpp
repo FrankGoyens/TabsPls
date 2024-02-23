62e717548f9132d733fd9f9b57f5cf3b97563096
Frank Goyens <frankgoyens@hotmail.com>
2024-02-23 23:54:49 +0100

    Fix bug where the EXECUTABLE argument was passed in the wrong encoding
    Would for instance happen on Windows (no native utf-8), and launching a
    file containing 'é', the url would not be launched and instead there
    would be a ShellError in the Debug output.

--- TabsPls_Qt/LightSpeedFileExplorer/EscapePodLauncher.cpp
+++ TabsPls_Qt/LightSpeedFileExplorer/EscapePodLauncher.cpp
@@ -12,7 +12,7 @@ namespace EscapePodLauncher {
 void LaunchUrlInWorkingDirectory(const QUrl& url, const FileSystem::Directory& workingDir) {
     const auto workingDirQString = FileSystem::StringConversion::FromRawPath(workingDir.path());
     const int status =
-        QProcess::execute(QString("EscapePod"), QStringList::fromVector({url.toString(), workingDirQString}));
+        QProcess::execute(QString("EscapePod"), QStringList::fromVector({url.toEncoded(), workingDirQString}));
 
     if (status != 0)
         qDebug() << "Something went wrong launching url " << url.toString() << " in directory " << workingDirQString;
