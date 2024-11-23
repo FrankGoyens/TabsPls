c107052cb7565264402653caa6d74255cc28707e
Frank Goyens <frankgoyens@hotmail.com>
2024-11-24 00:24:16 +0100

    Fix mistake where after changing tab order, the wrong tab is updated after a directory change

--- TabsPls_Qt/LightSpeedFileExplorer/TabsPlsMainWindow.hpp
+++ TabsPls_Qt/LightSpeedFileExplorer/TabsPlsMainWindow.hpp
@@ -13,19 +13,22 @@ namespace TabModel {
 struct Tab;
 }
 
+struct TabContainterItemImpl;
+
 class TabsPlsMainWindow : public QMainWindow {
     Q_OBJECT
 
   public:
     TabsPlsMainWindow(const QString& initialDirectory);
+    ~TabsPlsMainWindow();
 
   protected:
     void mousePressEvent(QMouseEvent*) override;
 
   private:
     QTabWidget* m_tabWidget = nullptr;
-    std::vector<std::shared_ptr<TabModel::Tab>> m_tabs;
+    std::vector<std::unique_ptr<TabContainterItemImpl>> m_tabs;
     std::vector<TabsPlsPython::Toolbar::Toolbar> m_toolbarModels;
 
-    std::shared_ptr<TabModel::Tab> OpenNewTab(const FileSystem::Directory&);
+    TabContainterItemImpl& OpenNewTab(const FileSystem::Directory&);
 };
